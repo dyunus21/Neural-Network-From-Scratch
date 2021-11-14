69b8da592f435021f7f0f8b249db35322b24af61
hhhzzzsss <hyzhou42@gmail.com>
2021-11-14 16:02:23 -0600

    Changed activation.hpp to util.hpp

--- includes/util.hpp
+++ includes/util.hpp
@@ -1,12 +1,14 @@
-#ifndef ACTIVATION_HPP
-#define ACTIVATION_HPP
+#ifndef UTIL_HPP
+#define UTIL_HPP
 
 #include "node.hpp"
 #include <vector>
 
-namespace Activation {
-    enum class ActivationFunction{relu};
+namespace Util {
+    enum class ActivationFunction{none, relu};
     void activate(ActivationFunction activationFunction, std::vector<Node*> nodes, int length);
+
+    enum class Initializer{xavier, he};
 }
 
 #endif
