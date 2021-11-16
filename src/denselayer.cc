77c157840c5d2ff710974ce7c111805c66b833db
znodnarb <fireblaze7683@gmail.com>
2021-11-16 17:17:48 -0600

    added forward propagation

--- src/denselayer.cc
+++ src/denselayer.cc
@@ -11,3 +11,24 @@ DenseLayer::DenseLayer(int size, Layer* previous):
 const std::vector<Layer*>& DenseLayer::getDependencies() const {
   return dependencies;
 }
+
+void DenseLayer::forward_propagate() {
+  // weights
+  for (int i = 0; i < shape.at(0); i++) {  // loop thru current layer
+    for (int j = 0; j < dependencies.at(0)->getShape().at(0);
+         j++) {  // loop thru prev layer
+      weights.forward_apply(&(dependencies.at(0)->getNodes()[j]),
+                            &nodes[i],
+                            i * dependencies.at(0)->getShape().at(0) + j);
+    }
+  }
+
+  // biases
+  for (int i = 0; i < shape.at(0); i++) {
+    biases.forward_apply(&nodes[i], i);
+  }
+}
+
+void DenseLayer::backward_propagate() {
+  // TODO
+}
--- src/inputlayer.cc
+++ src/inputlayer.cc
@@ -1,3 +1,7 @@
 #include "inputlayer.hpp"
 
 InputLayer::InputLayer(int size): Layer(size) {}
+
+void InputLayer::forward_propagate() {}  // THESE ARE SUPPOSED TO DO NOTHING
+
+void InputLayer::backward_propagate() {}
