bb438803490ab6e4f9676f4eb78846d988c48eb9
znodnarb <fireblaze7683@gmail.com>
2021-11-16 02:16:07 -0600

    created basic loss function

--- includes/util.hpp
+++ includes/util.hpp
@@ -1,14 +1,18 @@
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
-#include "node.hpp"
 #include <vector>
 
-namespace Util {
-    enum class ActivationFunction{none, relu};
-    void activate(ActivationFunction activationFunction, std::vector<Node*> nodes, int length);
+#include "node.hpp"
 
-    enum class Initializer{xavier, he};
-}
+namespace Util {
+  enum class ActivationFunction { none, relu };
+  void activate(ActivationFunction activationFunction,
+                std::vector<Node*> nodes,
+                int length);
+  float loss(std::vector<Node*> expected, std::vector<Node*> actual);
+
+  enum class Initializer { xavier, he };
+}  // namespace Util
 
 #endif
---
+++ src/util.cc
@@ -0,0 +1,18 @@
+#include "util.hpp"
+
+#include <cmath>
+#include <stdexcept>
+
+// utility function that calculates loss using least squared
+float Util::loss(std::vector<Node*> expected, std::vector<Node*> actual) {
+  if (expected.size() != actual.size()) {
+    throw std::runtime_error(
+        "Loss function needs identical dimensional layers");
+  }
+
+  float loss = 0;
+  for (int i = 0; i < expected.size(); i++) {
+    loss += pow(expected.at(i)->value - actual.at(i)->value, 2);
+  }
+  return loss;
+}
