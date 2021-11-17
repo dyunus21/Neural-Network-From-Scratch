d0215dbbaa4502cf10e0a8ce3ce6ddcacbd43bad
Diya Yunus <dyunus2@illinois.edu>
2021-11-17 15:14:04 -0600

    Added a Reader Class

--- driver.cc
+++ driver.cc
@@ -3,80 +3,19 @@
 #include<fstream>
 #include <stdexcept>
 #include "neuralnet.hpp"
+#include "reader.hpp"
 
-//declaration
-int* read_mnist_labels(std::string full_path, int& number_of_labels);
-int** read_mnist_images(std::string full_path, int& number_of_images, int& image_size);
 
+//setup driver file to have arguments
 int main() {
 
-    //test dataset
-    int numLabels = 10000;
-    int numImages = 10000;
-    int imageSize = 28*28;
-    int* labels_dataset = read_mnist_labels("tests/datasets/t10k-labels-idx1-ubyte", numLabels);
-    int** image_dataset = read_mnist_images("tests/datasets/t10k-images-idx3-ubyte", numImages, imageSize);
+    //sample for test dataset, later can modify to get data from command line
+    std::string label_path = "tests/datasets/t10k-labels-idx1-ubyte";
+    std::string image_path = "tests/datasets/t10k-images-idx3-ubyte";
+    Reader r(10000,10000,784);
+    int* labels_dataset = r.read_mnist_labels(label_path);
+    int** image_dataset = r.read_mnist_images(image_path);
+    std::cout<<labels_dataset[1];
     
 }
 
-
-int* read_mnist_labels(std::string full_path, int& number_of_labels) {
-    auto reverseInt = [](int i) {
-        unsigned char c1, c2, c3, c4;
-        c1 = i & 255, c2 = (i >> 8) & 255, c3 = (i >> 16) & 255, c4 = (i >> 24) & 255;
-        return ((int)c1 << 24) + ((int)c2 << 16) + ((int)c3 << 8) + c4;
-    };
-    std::ifstream file(full_path, std::ios::binary);
-
-    if(file.is_open()) {
-        int magic_number = 0;
-        file.read((char *)&magic_number, sizeof(magic_number));
-        magic_number = reverseInt(magic_number);
-
-        if(magic_number != 2049) throw std::runtime_error("Invalid MNIST label file!");
-
-        file.read((char *)&number_of_labels, sizeof(number_of_labels)), number_of_labels = reverseInt(number_of_labels);
-
-        int* _dataset = new int[number_of_labels];
-        for(int i = 0; i < number_of_labels; i++) {
-            file.read((char*)&_dataset[i], 1);
-        }
-        return _dataset;
-    } else {
-        throw std::runtime_error("Unable to open file `" + full_path + "`!");
-    }
-}
-
-int** read_mnist_images(std::string full_path, int& number_of_images, int& image_size) {
-    auto reverseInt = [](int i) {
-        unsigned char c1, c2, c3, c4;
-        c1 = i & 255, c2 = (i >> 8) & 255, c3 = (i >> 16) & 255, c4 = (i >> 24) & 255;
-        return ((int)c1 << 24) + ((int)c2 << 16) + ((int)c3 << 8) + c4;
-    };
-
-    std::ifstream file(full_path, std::ios::binary);
-
-    if(file.is_open()) {
-        int magic_number = 0, n_rows = 0, n_cols = 0;
-
-        file.read((char *)&magic_number, sizeof(magic_number));
-        magic_number = reverseInt(magic_number);
-
-        if(magic_number != 2051) throw std::runtime_error("Invalid MNIST image file!");
-
-        file.read((char *)&number_of_images, sizeof(number_of_images)), number_of_images = reverseInt(number_of_images);
-        file.read((char *)&n_rows, sizeof(n_rows)), n_rows = reverseInt(n_rows);
-        file.read((char *)&n_cols, sizeof(n_cols)), n_cols = reverseInt(n_cols);
-
-        image_size = n_rows * n_cols;
-
-        int** _dataset = new int*[number_of_images];
-        for(int i = 0; i < number_of_images; i++) {
-            _dataset[i] = new int[image_size];
-            file.read((char *)_dataset[i], image_size);
-        }
-        return _dataset;
-    } else {
-        throw std::runtime_error("Cannot open file `" + full_path + "`!");
-    }
-}
