9deb6665048a94983381ddaaff57a40ae27a1e3d
hhhzzzsss <hyzhou42@gmail.com>
2021-12-04 17:26:28 -0600

    Added batch size to optimizer

--- includes/optimizer.hpp
+++ includes/optimizer.hpp
@@ -4,6 +4,9 @@
 class Optimizer {
     public:
         virtual void optimize(float* weights, float* gradients, int size) = 0;
+        void set_batch_size(int size) { batch_size = size; };
+    protected:
+        int batch_size = 1;
 };
 
 #endif
